25110c41e73cb32598d539482b2be85ec3f479ff
Neeraj <neeraj.viman@gmail.com>
2014-10-09 23:23:36 +0530

    Initial commit!

---
+++ BookmarkImporter/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BookmarkImporter.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FileUpload ID="fuBookmark" runat="server" />
+        <asp:Button ID="btnUpload" runat="server" Text="Utha lo!" OnClick="btnUpload_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ BookmarkImporter/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BookmarkImporter
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            string folderPath = Server.MapPath("~/bookmarks/");
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            string newName = Guid.NewGuid().ToString();
+            string fileExtn = Path.GetExtension(fuBookmark.FileName);
+            string newFileName = newName + fileExtn;
+            string filePath = folderPath+newFileName;
+            fuBookmark.SaveAs(filePath);
+
+            string bookmarks = ReadBookmarks(filePath);
+
+            Response.Write(bookmarks);
+        }
+
+        private string ReadBookmarks(string filePath)
+        {
+            List<Bookmark> lstBookmark = new List<Bookmark>();
+
+            HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+
+            //// There are various options, set as needed
+            //htmlDoc.OptionFixNestedTags = true;
+
+            // filePath is a path to a file containing the html
+            htmlDoc.Load(filePath, true);
+
+            // ParseErrors is an ArrayList containing any errors from the Load statement
+            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
+            {
+                // Handle any parse errors as required
+
+            }
+            else
+            {
+                var docNode = htmlDoc.DocumentNode;
+
+                foreach (HtmlNode link in docNode.Descendants("A"))
+                {
+                    Bookmark objBook = new Bookmark();
+
+                    objBook.href = link.Attributes["HREF"].Value;
+                    objBook.title = link.InnerText;
+                    objBook.add_date = link.Attributes["ADD_DATE"].Value;
+
+                    lstBookmark.Add(objBook);
+                }                
+            }
+
+            return JsonConvert.SerializeObject(lstBookmark);
+        }
+
+        private class Bookmark
+        {
+            public string href { get; set; }
+            public string title { get; set; }
+            public string add_date { get; set; }
+        }
+    }
+}
